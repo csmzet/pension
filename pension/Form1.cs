341a201863e6df0458ead8cbb8b6c5803b64cacc
csmzet <csmzetkfu@mail.ru>
2016-09-02 12:35:21 +0300

    init

---
+++ pension/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+//using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Safari;
+using OpenQA.Selenium.Safari.Internal;
+using OpenQA.Selenium.Safari.Internal.Handlers;
+
+namespace pension
+{
+    public partial class Form1 : Form
+    {
+        private int width1280 = 1280;
+        private int width600 = 600;
+        private int width480 = 480;
+        private int width320 = 320;
+
+        private string firefoxNameVersion = "firefox_v47.0.1";
+        private string safariNameVersion = "safari_v5.1.7";
+        private string IENameVersion = "ie_v11";
+        private string chromeNameVersion = "chrome_v53";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //************************* Firefox (Работает) ****************************************************
+            //IWebDriver driverFirefox = new FirefoxDriver();
+            //driverFirefox.Manage().Timeouts().SetPageLoadTimeout(TimeSpan.FromSeconds(30));
+            //driverFirefox.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(55));
+            //driverFirefox.Manage().Timeouts().SetScriptTimeout(TimeSpan.FromSeconds(30));
+
+            //driverWork driverFirefoxWork = new driverWork(driverFirefox);
+            //driverFirefoxWork.autoTest(firefoxNameVersion, width1280);
+            //driverFirefoxWork.autoTest(firefoxNameVersion, width600);
+            //driverFirefoxWork.autoTest(firefoxNameVersion, width480);
+            //driverFirefoxWork.autoTest(firefoxNameVersion, width320);
+            //driverFirefox.Close();
+            //************************* Firefox ****************************************************
+
+
+            //************************* Safari 5.1.7 только на версии Selenium 2.47 *****************************************************
+            /*IWebDriver driverSafari = new SafariDriver();
+            driverSafari.Manage().Timeouts().SetPageLoadTimeout(TimeSpan.FromSeconds(30));
+            driverSafari.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(55));
+            driverSafari.Manage().Timeouts().SetScriptTimeout(TimeSpan.FromSeconds(30));
+
+            driverWork driverSafariWork = new driverWork(driverSafari);
+            driverSafariWork.autoTest(safariNameVersion, width1280);
+            driverSafariWork.autoTest(safariNameVersion, width600);
+            driverSafariWork.autoTest(safariNameVersion, width480);
+            driverSafariWork.autoTest(safariNameVersion, width320);
+            driverSafari.Close();*/
+            //************************* Safari 5.1.7 *******************************************************
+
+
+            //************************* IE11 (Работает) *****************************************************
+            IWebDriver driverIE = new InternetExplorerDriver();
+            driverIE.Manage().Timeouts().SetPageLoadTimeout(TimeSpan.FromSeconds(30));
+            driverIE.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(55));
+            driverIE.Manage().Timeouts().SetScriptTimeout(TimeSpan.FromSeconds(30));
+
+            driverWork driverIEWork = new driverWork(driverIE);
+            driverIEWork.autoTest(IENameVersion, width1280);
+            driverIEWork.autoTest(IENameVersion, width600);
+            driverIEWork.autoTest(IENameVersion, width480);
+            driverIEWork.autoTest(IENameVersion, width320);
+            driverIE.Close();
+            //************************* IE11 *******************************************************
+
+
+
+            //************************* chrome_v53 ( Работает и на 2,47) *****************************************************
+            //IWebDriver driverChrome = new ChromeDriver();
+            //driverChrome.Manage().Timeouts().SetPageLoadTimeout(TimeSpan.FromSeconds(30));
+            //driverChrome.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(55));
+            //driverChrome.Manage().Timeouts().SetScriptTimeout(TimeSpan.FromSeconds(30));
+
+            //driverWork driverChromeWork = new driverWork(driverChrome);
+            //driverChromeWork.autoTest(chromeNameVersion, width1280);
+            //driverChromeWork.autoTest(chromeNameVersion, width600);
+            //driverChromeWork.autoTest(chromeNameVersion, width480);
+            //driverChromeWork.autoTest(chromeNameVersion, width320);
+            //driverChrome.Close();
+            //************************* chrome_v53 *******************************************************
+        }
+    }
+}
---
+++ pension/driverWork.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Safari;
+using OpenQA.Selenium.IE;
+using System.Drawing;
+using System.Threading;
+
+namespace pension
+{
+    class driverWork
+    {
+        //поле
+        IWebDriver driver { get; set; }
+        private string title = "title";
+        private string age = "age";
+        private string question1 = "question1";
+        private string question2 = "question2";
+        private string question3 = "question3";
+        private string final = "final";
+        private string finalCalculator = "final-calculator";
+
+        public driverWork(IWebDriver Driver)
+        {
+            driver = Driver;
+        }
+
+        public void ScreenBuild(string browserName, int screenWidth, string pageName)
+        {
+            Screenshot s1 = ((ITakesScreenshot)driver).GetScreenshot();
+            string fullPath = browserName + "/" + pageName + "@" + screenWidth + ".jpeg";
+            //string fullPath = screenWidth + ".jpeg";
+            s1.SaveAsFile(fullPath, System.Drawing.Imaging.ImageFormat.Jpeg);
+        }
+
+        public void autoTest(string browserName, int screenWidth)
+        {
+            driver.Navigate().GoToUrl("http://wsh1490.web-shop-hosting.com/vorfreude_result.de/index-if6.html");
+            driver.Manage().Window.Size = new Size(screenWidth, 2048);
+
+            driverWork driverWork = new driverWork(driver);
+
+            driverWork.ScreenBuild(browserName, screenWidth, title);
+
+            IWebElement button = driver.FindElement(By.Id("TitleCardStartButton"));
+            button.Click();
+            Thread.Sleep(3000);
+            driverWork.ScreenBuild(browserName, screenWidth, age);
+            button = driver.FindElement(By.Id("QuestionAgeWeiter"));
+            button.Click();
+            Thread.Sleep(3000);
+
+            driverWork.ScreenBuild(browserName, screenWidth, question1);
+            button = driver.FindElement(By.Id("QuestionFormWeiter"));
+            //<a id="QuestionFormWeiter" href="#">Weiter</a>
+            button.Click();
+
+            Thread.Sleep(3000);
+            //button = driver.FindElement(By.TagName("span")).FindElement(By.LinkText("+"));
+            button = driver.FindElement(By.XPath("/html/body/section/div[3]/div/div[2]/div[2]/div[2]/table/tbody/tr/td[3]/span"));
+
+            button.Click();
+            button.Click();
+            Thread.Sleep(2000);
+            driverWork.ScreenBuild(browserName, screenWidth, question2);
+            button = driver.FindElement(By.Id("QuestionFormWeiter"));
+            button.Click();
+
+            Thread.Sleep(3000);
+            button = driver.FindElement(By.XPath("/html/body/section/div[3]/div/div[2]/div[2]/div[3]/table/tbody/tr/td[3]/span"));
+            button.Click();
+            button.Click();
+            button.Click();
+            button.Click();
+            Thread.Sleep(3000);
+            driverWork.ScreenBuild(browserName, screenWidth, question3);
+            button = driver.FindElement(By.Id("QuestionFormWeiter"));
+            button.Click();
+
+            Thread.Sleep(3000);
+            driverWork.ScreenBuild(browserName, screenWidth, final);
+            button = driver.FindElement(By.XPath("/html/body/section/div[4]/div/div[5]/div/div/a"));
+            button.Click();
+
+            Thread.Sleep(3000);
+            driverWork.ScreenBuild(browserName, screenWidth, finalCalculator);
+        }
+    }
+}
